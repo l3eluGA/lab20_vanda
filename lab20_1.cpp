357ca6e54cea4f0e8345f6e02e3b97cd17982b0d
Vanda Rungthip <110966223+l3eluGA@users.noreply.github.com>
2023-02-21 17:12:40 +0700

    done

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -5,8 +5,10 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect R1,Rect R2)
+{
+	double w0=min((R2.x+R2.w),(R1.x+R1.w))-max(R1.x,R2.x);
+	double h0=min(R1.y,R2.y)-max((R1.y-R1.h),(R2.y-R2.h));
+	if(w0<0 or h0<0) return 0;
+	else return w0*h0;
 }
